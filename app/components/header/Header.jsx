768b15a640156b3cf2b9b2a657bf601311d8648e
ronlataza <ronryan.lataza@gmail.com>
2025-06-24 00:21:08 +0800

    adding header

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -5,7 +5,16 @@ import React from 'react'
 const Header = () => {
   return (
     <header className={`container ${styles.header}`}>
-        <div className={styles.headerLeft}></div>
+        <div className={styles.headerLeft}>
+            <h1>Get your Business online right away</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur atque itaque et quia.
+            </p>
+            <div className={styles.headerCta}>
+                <Link href = "/portfolio" className='btn'>Projects</Link>
+                <Link href = "/portfolio" className='btn'>Projects</Link>
+            </div>
+        </div>
         <div className={styles.headerRight}></div>
 
     </header>
