d49575a38ed985f82e3bd55be0342a63e7bc38db
ronlataza <ronryan.lataza@gmail.com>
2025-06-30 23:23:22 +0800

    adding codes

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -25,13 +25,13 @@ const Header = () => {
           <article classname ={styles.card1}>
             <Image src ={Image1} alt=''></Image>
           </article>
-           <article classname ={styles.card1}>
+           <article classname ={styles.card2}>
             <Image src ={Image2} alt=''></Image>
           </article>
-           <article classname ={styles.card1}>
+           <article classname ={styles.card3}>
             <Image src ={Image3} alt=''></Image>
           </article>
-           <article classname ={styles.card1}>
+           <article classname ={styles.card4}>
             <Image src ={Image4} alt=''></Image>
           </article>
         </div>
