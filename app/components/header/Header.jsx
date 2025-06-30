9b6c473ac1d16dbb753041a39be9515162f6507b
ronlataza <ronryan.lataza@gmail.com>
2025-06-30 23:44:45 +0800

    adding design

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -22,16 +22,16 @@ const Header = () => {
             </div>
         </div>
         <div className={styles.headerRight}>
-          <article classname ={styles.card1}>
+          <article className ={styles.photos1}>
             <Image src ={Image1} alt=''></Image>
           </article>
-           <article classname ={styles.card2}>
+           <article className ={styles.photos2}>
             <Image src ={Image2} alt=''></Image>
           </article>
-           <article classname ={styles.card3}>
+           <article className ={styles.photos3}>
             <Image src ={Image3} alt=''></Image>
           </article>
-           <article classname ={styles.card4}>
+           <article className ={styles.photos4}>
             <Image src ={Image4} alt=''></Image>
           </article>
         </div>
