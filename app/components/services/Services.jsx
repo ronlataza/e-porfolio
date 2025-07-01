6bb1ea532e135371ccccf003c95458215231d4ba
ronlataza <ronryan.lataza@gmail.com>
2025-07-01 20:01:36 +0800

    adding design

---
+++ app/components/services/Services.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
+import { MdDesignServices } from "react-icons/md"
+import { FaCode } from "react-icons/fa"
+import { FaServer } from "react-icons/fa"
+import { FaLaptopCode } from "react-icons/fa6"
+import styles from './services.module.css'
+
+
+
+const Services = () => {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
