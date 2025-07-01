40b316e5eb9a0b6a6abd979ff26fa680c4353162
ronlataza <ronryan.lataza@gmail.com>
2025-07-01 21:29:59 +0800

    adding design

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
+"use client"
+
 import React from 'react'
 import styles from './footer.module.css'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const data = [
+  {id: 1, link: '/', caption: 'Home'},
+  {id: 2, link: '/about', caption: 'About'},
+  {id: 3, link: '/services', caption: 'Services'},
+  {id: 4, link: '/portfolio', caption: 'Portfolio'},
+  {id: 5, link: '/contact', caption: 'Contact'},
+]
+
+
 const Footer = () => {
+  const pathname = usePathname;
   return (
-    <div>Footer</div>
+    <footer className={styles.footer}>
+      <div className={`container ${ styles.footerContainer}`}>
+        <Link href="/" className={styles.footerLogo} >Profile</Link>
+       <ul className={styles.footerNavItems}>
+          {
+            data.map(({id, link, caption})=> <li key={id}>
+              <Link href ={link} className={ pathname === link ? "active" : "" }>{caption}</Link>
+            </li>)
+          }
+
+        </ul>
+      </div>
+    </footer>
   )
 }
 
