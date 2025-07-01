0c2d7283038338b7d9a34ff48e96b6cac6aef5da
ronlataza <ronryan.lataza@gmail.com>
2025-07-01 20:59:22 +0800

    adding design

--- app/components/testimonials/Testimonials.jsx
+++ app/components/testimonials/Testimonials.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+"use client"
 
+import React from 'react'
+import { Pagination } from 'swiper/modules';
+import {Swiper , SwiperSlide} from 'swiper/react'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import styles from './testimonials.module.css'
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section className={StyleSheet.section}>
+        <h2>Testimonials</h2>
+        <p>Check out what clients says</p>
+         <Swiper
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={50}
+      slidesPerView={1}
+
+      pagination={{ clickable: true }}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+    <div className="swiper-pagination"></div>
+    </Swiper>
+    
+    </section>
   )
 }
 
