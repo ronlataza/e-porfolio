693d954b408318a1871de4b325db5b88ef17fd66
ronlataza <ronryan.lataza@gmail.com>
2025-07-02 20:36:23 +0800

    adding design

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './about.module.css'
 import SectionHeader from '../components/section-header/SectionHeader'
-import AboutImage from '../assets/about.JPG'
+import AboutImage from '../assets/about.jpg'
+import Image from 'next/image'
 const page = () => {
   return (
     <section>
@@ -11,15 +12,18 @@ const page = () => {
         <Image src ={ AboutImage} alt="My Image"></Image>
       </article>
       <article className={styles.aboutContent}> 
-        <h4>Hi, I'm Ron Ryan Lataza,
-           a graduate with a Bachelor of Science in Information Technology. 
+        <h4>Hi, I'm Ron Ryan Lataza</h4>
+        <p> a graduate with a Bachelor of Science in Information Technology. 
            I'm passionate about technology and constantly driven to learn, grow, 
            and innovate in the ever-evolving tech industry. With a strong foundation in IT and a curious, 
            detail-oriented mindset, I thrive on solving problems and exploring new tools and trends.
             I'm eager to contribute to meaningful projects, collaborate with forward-thinking teams, 
-            and continue developing my skills to make a lasting impact in the digital world.</h4>
+            and continue developing my skills to make a lasting impact in the digital world</p>
+            <a href ="../assets/cv.pdf" className={`btn primary ${styles.aboutCta}`} download rel="noopener noreferrer">
+            Download CV </a>
 
       </article>
+
     </div>
 
     </section>
